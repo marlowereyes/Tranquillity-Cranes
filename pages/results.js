0221f9a9fe3f69b6aef23ab24a5a0c0f9964edf4
Brian Paragas <brianparagas49@gmail.com>
2024-04-07 18:50:16 -0700

    Added navigation bar to results page

--- pages/results.js
+++ pages/results.js
@@ -1,12 +1,16 @@
 import { ResultsContainer } from "@/components/Results";
 import { useRouter } from "next/router"
 import { useState } from "react"
+import NavBar from "@/components/NavBar";
 
 export default function Results() {
     const {query} = useRouter();
     const resultId = query.resultId;
     return (
+        <>
         <ResultsContainer resultId={resultId} />
+        <NavBar/>
+        </>
     )
 }
 
