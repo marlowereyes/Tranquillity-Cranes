596ebb2f2a8a7c743d33cacc3c61c6353d89a8e6
Deluka Loku Gam Hewage <deluka.lgh@gmail.com>
2024-05-06 01:46:33 -0700

    adding static Calander

---
+++ components/Calander/index.js
@@ -0,0 +1,69 @@
+import styles from './Calander.module.css'
+
+export default function Calander() {
+
+    return(
+        <>
+            <div className={styles.calanderContainer}>
+                <div class={styles.weekend}>Sun</div>
+                <div class={styles.weekend}>Mon</div>
+                <div class={styles.weekend}>Tue</div>
+                <div class={styles.weekend}>Wed</div>
+                <div class={styles.weekend}>Thur</div>
+                <div class={styles.weekend}>Fri</div>
+                <div class={styles.weekend}>Sat</div>
+
+                <div class={styles.box}>1</div>
+                <div class={styles.box}>2</div>
+                <div class={styles.box}>3</div>
+                <div class={styles.box}>4</div>
+                <div class={styles.box}>5</div>
+                <div class={styles.box}>6</div>
+                <div class={styles.box}>7</div>
+                <div class={styles.box}>8</div>
+                <div class={styles.box}>9</div>
+                <div class={styles.box}>10</div>
+                <div class={styles.box}>11</div>
+                <div class={styles.box}>12</div>
+                <div class={styles.box}>13</div>
+                <div class={styles.box}>14</div>
+                <div class={styles.box}>15</div>
+                <div class={styles.box}>16</div>
+                <div class={styles.box}>17</div>
+                <div class={styles.box}>18</div>
+                <div class={styles.box}>19</div>
+                <div class={styles.box}>20</div>
+                <div class={styles.box}>21</div>
+                <div class={styles.box}>22</div>
+                <div class={styles.box}>22</div>
+                <div class={styles.box}>23</div>
+                <div class={styles.box}>24</div>
+                <div class={styles.box}>25</div>
+                <div class={styles.box}>26</div>
+                <div class={styles.box}>27</div>
+                <div class={styles.box}>28</div>
+                <div class={styles.box}>29</div>
+                <div class={styles.box}>30</div>
+                <div class={styles.box}>31</div>
+                <div class={styles.box}>32</div>
+                <div class={styles.box}>33</div>
+                <div class={styles.box}>34</div>
+                <div class={styles.box}>35</div>
+                <div class={styles.box}>36</div>
+                <div class={styles.box}>37</div>
+                <div class={styles.box}>38</div>
+                <div class={styles.box}>39</div>
+                <div class={styles.box}>40</div>
+                <div class={styles.box}>41</div>
+                <div class={styles.box}>42</div>
+                <div class={styles.box}>43</div>
+                <div class={styles.box}>44</div>
+                <div class={styles.box}>45</div>
+                <div class={styles.box}>46</div>
+                <div class={styles.box}>47</div>
+                <div class={styles.box}>48</div>
+                <div class={styles.box}>49</div>
+            </div>
+        </>
+    )
+}
---
+++ pages/activities.js
@@ -0,0 +1,10 @@
+import Calander from '@/components/Calander';
+import styles from '@/styles/Activities.module.css';
+
+export default function Activities() {
+    return(
+        <>
+            <Calander/>
+        </>
+    )
+}
